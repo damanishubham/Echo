701577fa0dcb7b93d376fdc161f5a43742754c1b
Shubham <damanishubham92@gmail.com>
2021-11-23 12:46:58 +0530

    Modified SongData file

--- app/src/main/java/com/example/echo/DataModels/SongData.kt
+++ app/src/main/java/com/example/echo/DataModels/SongData.kt
@@ -5,11 +5,34 @@ import android.os.Parcelable
 
 class Songs(val songID: Long, val songTitle :String, val songArtist : String,
                 val songData: String, val dateAdded: Long) : Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readLong()
+    ) {
+    }
+
     override fun describeContents(): Int {
-        TODO("Not yet implemented")
+        return  0
     }
 
     override fun writeToParcel(dest: Parcel?, flags: Int) {
-        TODO("Not yet implemented")
+        dest?.writeLong(songID)
+        dest?.writeString(songTitle)
+        dest?.writeString(songArtist)
+        dest?.writeString(songData)
+        dest?.writeLong(dateAdded)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Songs> {
+        override fun createFromParcel(parcel: Parcel): Songs {
+            return Songs(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Songs?> {
+            return arrayOfNulls(size)
+        }
     }
 }
